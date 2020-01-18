3e13cb29acb18d83102c6de081ee67e1214cd05f
Anton Sarmatin <antonsarmatin@googlemail.com>
2020-01-19 01:34:43 +0300

    base vm loading ld

--- app/src/main/java/pro/apir/tko/presentation/platform/BaseViewModel.kt
+++ app/src/main/java/pro/apir/tko/presentation/platform/BaseViewModel.kt
@@ -1,5 +1,6 @@
 package pro.apir.tko.presentation.platform
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import pro.apir.tko.core.exception.Failure
@@ -13,7 +14,16 @@ abstract class BaseViewModel : ViewModel() {
 
     var failure: MutableLiveData<Failure> = MutableLiveData()
 
+    private val _loading: MutableLiveData<Boolean> = MutableLiveData(false)
+    val loading: LiveData<Boolean>
+        get() = _loading
+
     protected fun handleFailure(failure: Failure) {
+        loading(false)
         this.failure.value = failure
+    }
+
+    protected fun loading(isLoading: Boolean) {
+        this._loading.value = isLoading
     }
 }
