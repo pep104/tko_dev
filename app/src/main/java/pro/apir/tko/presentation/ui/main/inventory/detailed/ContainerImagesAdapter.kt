54b1986c3baf6a0b6f30d31587521bdd85299307
antonsarmatin <antonsarmatin@googlemail.com>
2020-01-21 01:02:00 +0300

    glide image transformation
    multitransform

--- app/src/main/java/pro/apir/tko/presentation/ui/main/inventory/detailed/ContainerImagesAdapter.kt
+++ app/src/main/java/pro/apir/tko/presentation/ui/main/inventory/detailed/ContainerImagesAdapter.kt
@@ -6,12 +6,16 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 import kotlinx.android.synthetic.main.item_image_container_list.view.*
 import pro.apir.tko.R
 import pro.apir.tko.domain.model.ImageModel
 import pro.apir.tko.presentation.extension.dpToPx
+import pro.apir.tko.presentation.extension.pxToDp
 
 /**
  * Created by antonsarmatin
@@ -48,7 +52,10 @@ class ContainerImagesAdapter : RecyclerView.Adapter<ContainerImagesAdapter.Image
         }
 
         fun bind(item: ImageModel, pos: Int) {
-            Glide.with(imageView).load(imageView.context.getString(R.string.url_file, item.url)).transform(RoundedCornersTransformation(8.dpToPx, 0)).into(imageView)
+            Glide.with(imageView)
+                    .load(imageView.context.getString(R.string.url_file, item.url))
+                    .transform(CenterCrop(), RoundedCornersTransformation(8.dpToPx, 0))
+                    .into(imageView)
         }
 
     }
