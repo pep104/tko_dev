e4424273b1ecb6ad5eb12632e195781547c056db
antonsarmatin <antonsarmatin@googlemail.com>
2020-01-21 00:36:45 +0300

    dexter permission checker

--- app/src/main/java/pro/apir/tko/presentation/ui/main/menu/MenuFragment.kt
+++ app/src/main/java/pro/apir/tko/presentation/ui/main/menu/MenuFragment.kt
@@ -1,10 +1,17 @@
 package pro.apir.tko.presentation.ui.main.menu
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.fragment_menu.view.*
 import kotlinx.android.synthetic.main.toolbar_title.view.*
 import pro.apir.tko.R
@@ -23,6 +30,9 @@ class MenuFragment : BaseFragment() {
 
     override fun handleFailure() = viewModel.failure
 
+    private lateinit var btnInventory: MaterialButton
+    private lateinit var btnRoutes: MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         appComponent.createMainComponent().injectMenuFragment(this)
@@ -37,15 +47,30 @@ class MenuFragment : BaseFragment() {
 
         view.textToolbarTitle.text = getString(R.string.title_menu)
 
-        view.btnInventory.setOnClickListener {
+        btnInventory = view.btnInventory.apply { this.isEnabled = false }
+        btnRoutes = view.btnRoutes.apply { this.isEnabled = false }
+
+        btnInventory.setOnClickListener {
             findNavController().navigate(R.id.action_menuFragment_to_inventoryListFragment)
         }
 
-        view.btnRoutes.setOnClickListener {
+        btnRoutes.setOnClickListener {
             //TODO navigate to routes list
         }
 
-        //todo ask for permissions - location and file
+
+        Dexter.withActivity(activity).withPermissions(
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                btnRoutes.isEnabled = true
+                btnInventory.isEnabled = true
+            }
+
+            override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>?, token: PermissionToken?) {
+                token?.continuePermissionRequest()
+            }
+        }).check()
 
     }
 
