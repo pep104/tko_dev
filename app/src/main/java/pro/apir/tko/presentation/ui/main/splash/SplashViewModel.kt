26fd74c50939ae8c0865de14661a3433b994723f
antonsarmatin <antonsarmatin@googlemail.com>
2020-01-21 00:47:48 +0300

    splash viewmodel checks user auth state

--- app/src/main/java/pro/apir/tko/presentation/ui/main/splash/SplashViewModel.kt
+++ app/src/main/java/pro/apir/tko/presentation/ui/main/splash/SplashViewModel.kt
@@ -7,40 +7,34 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import pro.apir.tko.core.constant.KEY_ACCESS_TOKEN
 import pro.apir.tko.data.framework.manager.preferences.PreferencesManager
+import pro.apir.tko.data.framework.manager.token.TokenManager
 import pro.apir.tko.presentation.platform.BaseViewModel
 import pro.apir.tko.presentation.ui.main.GlobalState
 import javax.inject.Inject
 
-class SplashViewModel @Inject constructor(private val preferencesManager: PreferencesManager) : BaseViewModel() {
+class SplashViewModel @Inject constructor(private val tokenManager: TokenManager) : BaseViewModel() {
 
     private val _state = MutableLiveData<GlobalState.UserState>()
     val state: LiveData<GlobalState.UserState>
         get() = _state
 
-    //TODO Вынести логику
     init {
 
-        val aToken = preferencesManager.getString(KEY_ACCESS_TOKEN)
-        if (aToken.isNotBlank()) {
-            //TODO CHECK EXPIRATION
-            //TODO REAUTH
-            //TODO ???
-        } else {
-            //TODO LOGIN NEEDED
-        }
-
-        //TODO REMOVE
         viewModelScope.launch {
             delay(2000)
-            _state.postValue(GlobalState.UserState.LoginNeeded)
+            if (tokenManager.getRefreshToken().isNotBlank() && tokenManager.getAccessToken().isNotBlank() && !tokenManager.isRefreshTokenExpired()) {
+                _state.postValue(GlobalState.UserState.Authenticated)
+            } else {
+                _state.postValue(GlobalState.UserState.LoginNeeded)
+            }
+
         }
 
     }
 
-
-    sealed class Navigate {
-        object Main : Navigate()
-        object Login : Navigate()
+    private fun debug() {
+        _state.postValue(GlobalState.UserState.LoginNeeded)
     }
+
 
 }
