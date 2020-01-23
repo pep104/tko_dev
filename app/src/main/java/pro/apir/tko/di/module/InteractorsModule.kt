1609c117d186e451121fe9d6244a0daf37aa4b9e
antonsarmatin <antonsarmatin@googlemail.com>
2020-01-23 20:44:35 +0300

    added interactors module injection for address interactor

--- app/src/main/java/pro/apir/tko/di/module/InteractorsModule.kt
+++ app/src/main/java/pro/apir/tko/di/module/InteractorsModule.kt
@@ -2,8 +2,11 @@ package pro.apir.tko.di.module
 
 import dagger.Module
 import dagger.Provides
+import pro.apir.tko.data.repository.address.AddressRepository
 import pro.apir.tko.data.repository.auth.AuthRepository
 import pro.apir.tko.data.repository.inventory.InventoryRepository
+import pro.apir.tko.domain.interactors.address.AddressInteractor
+import pro.apir.tko.domain.interactors.address.AddressInteractorImpl
 import pro.apir.tko.domain.interactors.auth.AuthInteractor
 import pro.apir.tko.domain.interactors.auth.AuthInteractorImpl
 import pro.apir.tko.domain.interactors.inventory.InventoryInteractor
@@ -21,5 +24,9 @@ class InteractorsModule {
     @Provides
     @Singleton
     fun inventoryInteractor(inventoryRepository: InventoryRepository): InventoryInteractor = InventoryInteractorImpl(inventoryRepository)
+
+    @Provides
+    @Singleton
+    fun addressInteractor(addressRepository: AddressRepository): AddressInteractor = AddressInteractorImpl(addressRepository)
 
 }
