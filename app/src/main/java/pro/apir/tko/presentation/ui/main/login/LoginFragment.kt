d341449f64678b5f2edc2c48de7818086808d64c
antonsarmatin <antonsarmatin@googlemail.com>
2020-01-25 20:39:25 +0300

    login fragment statusbar light theme

--- app/src/main/java/pro/apir/tko/presentation/ui/main/login/LoginFragment.kt
+++ app/src/main/java/pro/apir/tko/presentation/ui/main/login/LoginFragment.kt
@@ -64,6 +64,8 @@ class LoginFragment : BaseFragment() {
         }
 
         observeViewModel()
+
+        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 
     private fun observeViewModel() {
