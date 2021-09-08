6984dfe687028c92572a6e0a2eb3068e5caa8802
antonsarmatin <antonsarmatin@googlemail.com>
2021-09-08 23:47:44 +0300

    TKO-0 AsyncListDiffer

--- app/src/main/java/pro/apir/tko/presentation/ui/main/list/inventory/ContainerListAdapter.kt
+++ app/src/main/java/pro/apir/tko/presentation/ui/main/list/inventory/ContainerListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_container_list.view.*
@@ -17,7 +18,23 @@ import pro.apir.tko.domain.model.ContainerAreaListModel
  */
 class ContainerListAdapter : RecyclerView.Adapter<ContainerListAdapter.ContainerHolder>() {
 
-    private val data = arrayListOf<ContainerAreaListModel>()
+
+    private val diffCallback = object : DiffUtil.ItemCallback<ContainerAreaListModel>() {
+        override fun areItemsTheSame(
+            oldItem: ContainerAreaListModel,
+            newItem: ContainerAreaListModel,
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ContainerAreaListModel,
+            newItem: ContainerAreaListModel,
+        ): Boolean {
+            return oldItem.id == newItem.id && oldItem.location == newItem.location && oldItem.registryNumber == newItem.registryNumber
+        }
+    }
+    private val differ = AsyncListDiffer(this, diffCallback)
 
     private var listener: OnItemClickListener? = null
 
@@ -28,11 +45,7 @@ class ContainerListAdapter : RecyclerView.Adapter<ContainerListAdapter.Container
     }
 
     fun setList(list: List<ContainerAreaListModel>) {
-        val diffCallback = ContainerDiffCallback(this.data, list)
-        val diffResult = DiffUtil.calculateDiff(diffCallback)
-        this.data.clear()
-        this.data.addAll(list)
-        diffResult.dispatchUpdatesTo(this)
+        differ.submitList(list)
     }
 
     fun setListener(listener: OnItemClickListener?) {
@@ -40,13 +53,14 @@ class ContainerListAdapter : RecyclerView.Adapter<ContainerListAdapter.Container
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContainerHolder {
-        return ContainerHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_container_list, parent, false))
+        return ContainerHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_container_list, parent, false))
     }
 
-    override fun getItemCount() = data.size
+    override fun getItemCount() = differ.currentList.size
 
     override fun onBindViewHolder(holder: ContainerHolder, position: Int) {
-        holder.bind(data[position], position)
+        holder.bind(differ.currentList[position], position)
     }
 
     inner class ContainerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -62,10 +76,15 @@ class ContainerListAdapter : RecyclerView.Adapter<ContainerListAdapter.Container
         fun bind(item: ContainerAreaListModel, pos: Int) {
 
             textHeader.text = item.location
-            val pluredCount = textMessage.context.resources.getQuantityString(R.plurals.plurals_containers, item.containersCount
-                    ?: 0, item.containersCount)
-            val area = item.area ?: 0.0
-            textMessage.text = textMessage.context.getString(R.string.text_list_container_message, item.registryNumber, pluredCount, area.toInt().toString())
+            val pluredCount =
+                textMessage.context.resources.getQuantityString(R.plurals.plurals_containers,
+                    item.containersCount,
+                    item.containersCount)
+            val area = item.area
+            textMessage.text = textMessage.context.getString(R.string.text_list_container_message,
+                item.registryNumber,
+                pluredCount,
+                area.toInt().toString())
 
             itemView.setOnClickListener {
                 listener?.onContainerItemClicked(item)
